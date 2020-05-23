177cb97182d59f43b201ebf0c384d3cd1f1e1b4a
odomar <severingosset@orange.fr>
2020-05-23 15:13:52 +0200

    fix indent

--- OpenGL/apps/SpringMass3D/main.cpp
+++ OpenGL/apps/SpringMass3D/main.cpp
@@ -118,40 +118,40 @@ std::vector<float> generateFlag(int x, int y) {
 
 int main(int argc, char **argv) {
     // Window size
-	const int W = 1200;
-	const int H = 800;
+    const int W = 1200;
+    const int H = 800;
 
-	// Init GLFW
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    // Init GLFW
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // Version 3.3
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Create a window
     GLFWwindow* window = glfwCreateWindow(W, H, "LearnOpenGL", nullptr, nullptr);
-      if (window == nullptr) {
-              std::cout << "Failed to create GLFW window" << std::endl;
-              glfwTerminate();
-              return -1;
-      }
+    if (window == nullptr) {
+            std::cout << "Failed to create GLFW window" << std::endl;
+            glfwTerminate();
+            return -1;
+    }
 
-      glfwMakeContextCurrent(window);
-      // Call the mouse callback function every frame
-      glfwSetCursorPosCallback(window, mouse_callback);
-      glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // using window resizing function
-      glfwSetScrollCallback(window, scroll_callback);
-      // Capture the cursor and disable it
-      glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwMakeContextCurrent(window);
+    // Call the mouse callback function every frame
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // using window resizing function
+    glfwSetScrollCallback(window, scroll_callback);
+    // Capture the cursor and disable it
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-      // Init Glad
-      if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-              std::cout << "Failed to initialize GLAD" << std::endl;
-              return -1;
-      }
+    // Init Glad
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+            std::cout << "Failed to initialize GLAD" << std::endl;
+            return -1;
+    }
 
-      Shader shader("bin/shaders/SpringMass3D/3DColor.vs.glsl", "bin/shaders/SpringMass3D/3DColor.fs.glsl");
+    Shader shader("bin/shaders/SpringMass3D/3DColor.vs.glsl", "bin/shaders/SpringMass3D/3DColor.fs.glsl");
 
-      /* ----------Initializations----------- */
+    /* ----------Initializations----------- */
         
     // Generate flag
     int sizeX = 48;
@@ -182,60 +182,60 @@ int main(int argc, char **argv) {
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
-	glEnable(GL_DEPTH_TEST);
+    glEnable(GL_DEPTH_TEST);
 
-	shader.use();
-	int modelLoc = glGetUniformLocation(shader.ID, "model");
-	int viewLoc = glGetUniformLocation(shader.ID, "view");
-	int projectionLoc = glGetUniformLocation(shader.ID, "projection");
-	glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)W / (float)H, 0.1f, 1000.0f);
-	glm::mat4 view = camera.GetViewMatrix();
-	glm::vec3 Color(1.0f, 0.5f, 0.31f);
+    shader.use();
+    int modelLoc = glGetUniformLocation(shader.ID, "model");
+    int viewLoc = glGetUniformLocation(shader.ID, "view");
+    int projectionLoc = glGetUniformLocation(shader.ID, "projection");
+    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)W / (float)H, 0.1f, 1000.0f);
+    glm::mat4 view = camera.GetViewMatrix();
+    glm::vec3 Color(1.0f, 0.5f, 0.31f);
 
-	//Draw line mode
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //Draw line mode
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-	/* ----------Rendering loop----------- */
-	while(!glfwWindowShouldClose(window)) {
-        // rendering commands here
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		float currentFrame = (float)glfwGetTime();
-		deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;
+    /* ----------Rendering loop----------- */
+    while(!glfwWindowShouldClose(window)) {
+    // rendering commands here
+            glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            float currentFrame = (float)glfwGetTime();
+            deltaTime = currentFrame - lastFrame;
+            lastFrame = currentFrame;
 
-		//
-        // TODO : Update vertices according masses with physics
-        //
+            //
+    // TODO : Update vertices according masses with physics
+    //
 
-		// Input processing
-		processInput(window);
-		// Update camera position
-		projection = glm::perspective(glm::radians(camera.Zoom), (float)W / (float)H, 0.1f, 1000.0f);
-		shader.setMat4("projection", projection);
-		view = camera.GetViewMatrix();
-		shader.setMat4("view", view);
-        // Set color
-		shader.setVec3("Color", Color);
+            // Input processing
+            processInput(window);
+            // Update camera position
+            projection = glm::perspective(glm::radians(camera.Zoom), (float)W / (float)H, 0.1f, 1000.0f);
+            shader.setMat4("projection", projection);
+            view = camera.GetViewMatrix();
+            shader.setMat4("view", view);
+    // Set color
+            shader.setVec3("Color", Color);
 
-		// Draw flag
-        glBindVertexArray(VAO);
-		glm::mat4 model = glm::mat4(1.0f);
-		shader.setMat4("model", model);
-		glDrawArrays(GL_TRIANGLES, 0, flagSize * 18);
+            // Draw flag
+    glBindVertexArray(VAO);
+            glm::mat4 model = glm::mat4(1.0f);
+            shader.setMat4("model", model);
+            glDrawArrays(GL_TRIANGLES, 0, flagSize * 18);
 
-		//
-        // TODO : Update the masses position according vertices
-        //
+            //
+    // TODO : Update the masses position according vertices
+    //
 
-		// check and call events and swap the buffers
-		glfwPollEvents();
-		glfwSwapBuffers(window);
-	}
+            // check and call events and swap the buffers
+            glfwPollEvents();
+            glfwSwapBuffers(window);
+    }
 
-	glDeleteVertexArrays(1, &VAO);
+    glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
 
-	glfwTerminate();
-	return 0;
+    glfwTerminate();
+    return 0;
 }
